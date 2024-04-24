6d5fe1f7eec41926845c1aa21ebeb0ea3916c70c
Gabrihelln <bihellaraujo@gmail.com>
2024-04-23 21:11:26 -0300

    config server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from "cors"
 import express from "express"
 import mongoose from 'mongoose'
 import helmet from "helmet"
+import dbConnection from './dbConfig/index.js'
+import morgan from 'morgan'
 
 
 dotenv.config()
@@ -11,3 +13,20 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+dbConnection ()
+
+
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10mb"}))
+app.use(express.urlencoded({ extended: true}))
+app.use(morgan("dev"))
+
+// app.use(router)
+// app.use(errorMiddleware())
+app.listen(PORT, () => {
+    console.log("Server running of port " + PORT);
+  });
